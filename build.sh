c824462e702e6d8694a13053f69174ca877d3950
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2025-12-08 08:46:56 -0500

    inital commit: working

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+./unix/configure
+make
+
+mkdir -p $CONDA_PREFIX/imager
+cp ds9/unix/ds9base $CONDA_PREFIX/imager/ds9
+
+mkdir -p $CONDA_PREFIX/lib/ds9
+cp -r ds9/unix/mntpt/* $CONDA_PREFIX/lib/ds9
+
+cp ds9/unix/ds9.script $CONDA_PREFIX/bin/ds9
+chmod +x $CONDA_PREFIX/bin/ds9
