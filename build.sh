2ffbff18dfad1f315299ebd6a06fab1eddc93834
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2025-12-10 09:30:47 -0500

    tweaks related to workingon osx-arm

--- build.sh
+++ build.sh
@@ -3,14 +3,13 @@
 
 #PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${PREFIX}/share/pkgconfig"
 
-./unix/configure
-make
+(unset host_alias; unset build_alias; ./unix/configure; make)
 
-mkdir -p $CONDA_PREFIX/imager
-cp ds9/unix/ds9base $CONDA_PREFIX/imager/ds9
+mkdir -p $PREFIX/imager
+cp ds9/unix/ds9base $PREFIX/imager/ds9
 
-mkdir -p $CONDA_PREFIX/lib/ds9
-cp -r ds9/unix/mntpt/* $CONDA_PREFIX/lib/ds9
+mkdir -p $PREFIX/lib/ds9
+cp -r ds9/unix/mntpt/* $PREFIX/lib/ds9
 
-cp ds9/unix/ds9.script $CONDA_PREFIX/bin/ds9
-chmod +x $CONDA_PREFIX/bin/ds9
+cp ds9/unix/ds9.script $PREFIX/bin/ds9
+chmod +x $PREFIX/bin/ds9
